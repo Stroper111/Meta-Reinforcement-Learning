3dd2075b09e49d5304ef8438f211e0d24e2d0f04
Thymen <t.d.j.rijpkema@gmail.com>
2019-12-10 20:40:06 +0100

    added test path, so now will laways remove temp model files (independent of where the test is run from.)

--- tests/model/test_simple_model.py
+++ tests/model/test_simple_model.py
@@ -40,28 +40,29 @@ class TestSimpleModel(unittest.TestCase):
         cls.save_msg = cls.model.save_msg
         cls.back_up_count = cls.model.back_up_count
 
+        cls.full_path_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "temp")
+
     @classmethod
     def tearDownClass(cls) -> None:
-        for file in glob.glob(os.path.join("temp", "*.h5")):
+        for file in glob.glob(os.path.join(cls.full_path_directory, "*.h5")):
             os.remove(file)
 
-        if os.path.exists("temp"):
-            os.removedirs("temp")
+        if os.path.exists(cls.full_path_directory):
+            os.removedirs(cls.full_path_directory)
 
     def test_init(self):
         self.assertEqual(self.input_shape, self.model.input_shape, "Wrong input dimensions")
         self.assertEqual(self.action_space, self.model.action_space, "Wrong action space")
 
     def test_saving_loading(self):
-        full_path_directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "temp")
-        self.model.save_model(full_path_directory)
-        self.model.load_model(full_path_directory)
+        self.model.save_model(self.full_path_directory)
+        self.model.load_model(self.full_path_directory)
 
         for episode in range(15):
             self.model.episodes = episode * 100
             self.model.frames = episode * 4048
-            self.model.save_checkpoint(full_path_directory)
+            self.model.save_checkpoint(self.full_path_directory)
 
-        saved_models = glob.glob(os.path.join(full_path_directory, "*weights.h5"))
+        saved_models = glob.glob(os.path.join(self.full_path_directory, "*weights.h5"))
         self.assertEqual(self.back_up_count, len(saved_models), "The back_up count is not set correctly")
-        self.model.load_checkpoint(full_path_directory)
+        self.model.load_checkpoint(self.full_path_directory)
