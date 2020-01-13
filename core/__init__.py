8a49a4ac532fa506d1093d3836af454230c46543
stroper111 <t.d.j.rijpkema@gmail.com>
2020-01-13 14:05:24 +0100

    Rename of replay memrory to base replay memory

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,6 @@
 import core.preprocessing.wrappers as wrappers
 
-from core.memory.replay_memory import ReplayMemory
+from core.memory.base_replay_memory import ReplayMemory
 from core.memory.sampling import BaseSampling
 from core.preprocessing import BasePreProcessing, BasePreProcessingGym, PreProcessingHvasslab, PreProcessingDeempind
 from core.agents import BaseAgent, BaseAgentGym
