8645126af6371be10ceae100abef3f8829492274
stroper111 <t.d.j.rijpkema@gmail.com>
2020-01-15 16:21:52 +0100

    Test case for the rescaling gray function

---
+++ tests/preprocessing/wrappers/test_rescaling.py
@@ -0,0 +1,54 @@
+import unittest
+import pickle
+import numpy as np
+import time
+import os
+
+from PIL import Image
+
+from core.preprocessing.wrappers.rescaling_gray import Rescaling
+
+
+class TestRescaling(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.setup = dict(coinrun=12)
+        cls.new_shape = (200, 40)
+        cls.wrapper = Rescaling(cls.setup, new_shape=cls.new_shape)
+
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+
+        with open(os.path.join(current_directory, "_original_images.pkl"), "rb") as file:
+            cls.images = pickle.load(file)['rgb']
+
+        # with open(os.path.join(current_directory, "_original_images_rescale.pkl"), "rb") as file:
+        #     cls.image_processed = pickle.load(file)
+
+    def test_process(self):
+        gray = self._rgb2_gray(self.images)
+        rescaled = self.wrapper.process(gray)
+        x, y = self.new_shape
+        image = np.reshape(rescaled, newshape=(x * 3, y * 4))
+        self.assertEqual((x * 3, y * 4), image.shape, "Processed image is not the same as stored")
+
+    def _show_saved_image(self):
+        """ Not a test method, but created for when a user wants to see input/output.  """
+        image = np.reshape(self.images, newshape=(64 * 3, 64 * 4, 3))
+        self._show_image(image, wait_time=3)
+
+    @staticmethod
+    def _show_image(image, wait_time):
+        """ Helper to depict an image for a wait time number of seconds.  """
+        img = Image.fromarray(image)
+        img.show()
+        time.sleep(wait_time)
+        img.close()
+
+    def _rgb2_gray(self, img):
+        gray = np.einsum("ijkl, l -> ijk", img, np.array([0.2126, 0.7152, 0.00722]))
+        return gray
+
+
+if __name__ == '__main__':
+    unittest.main()
