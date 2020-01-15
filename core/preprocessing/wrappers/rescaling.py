4f68097e07d64b616c0909d50bf28d821fe88f4f
stroper111 <t.d.j.rijpkema@gmail.com>
2020-01-15 15:42:12 +0100

    Rescaling now accept 2D and 3D images

--- core/preprocessing/wrappers/rescaling.py
+++ core/preprocessing/wrappers/rescaling.py
@@ -4,9 +4,9 @@ import PIL.Image
 from core.preprocessing.wrappers import BaseWrapper
 
 
-class RescalingGray(BaseWrapper):
+class Rescaling(BaseWrapper):
     """
-        Converts a gray image to a new dimension
+        Converts an image from 2D or 3D to a new dimension
 
         setup: dict
             The games as key and the instances as values , required for some wrappers.
@@ -18,8 +18,15 @@ class RescalingGray(BaseWrapper):
     def __init__(self, setup, new_shape=(105, 80)):
         super().__init__(setup)
 
-        # Size of each image in the state. Reversed order used by PIL.Image.
-        self.new_shape = tuple(reversed(new_shape))
+        if len(new_shape) == 2:
+            # Size of each image in the state. Reversed order used by PIL.Image.
+            self.new_shape = tuple(reversed(new_shape))
+        elif len(new_shape) == 3:
+            # Size of each image in the state. Reversed order used by PIL.Image.
+            x, y, z = new_shape
+            self.new_shape = tuple(y, x, z)
+        else:
+            raise ValueError("Only 2D or 3D conversions are allowed.")
 
     def step(self, action):
         img, *args = self.env.step(action)
