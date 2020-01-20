corrected model training and loss calculation.

core/agents/base_multi_env.py
@@ -48,7 +48,8 @@ class BaseAgentMultiEnv(BaseAgent):
     def _create_samplers(self):
         samplers = []
         for k in range(self.instances):
-            samplers.append(BaseSamplingMultiEnv(self.memories[k], batch_size=128))
+            samplers.append(BaseSamplingMultiEnv(self.memories[k], model=self.model, alpha=0.1,
+                                                 gamma=0.9, batch_size=128))
         return samplers
 
     def _create_loss(self):
@@ -73,13 +74,13 @@ class BaseAgentMultiEnv(BaseAgent):
                 self.memories[k].add(state=images['rgb'][k], action=actions[k], reward=rewards[k], end_episode=dones[k])
 
             for k in range(self.instances):
-                if self.memories[k].pointer_ratio() >= self.samplers[k].batch_size:
+                if self.memories[k].pointer_ratio() >= self.samplers[k].batch_size / self.memories[k].size:
                     self.loss[k].append(self.model.train_once(sampling=self.samplers[k]))
-                    # self.model.save_checkpoint(self.save_dir, episode, steps * self.instances)
 
             if update:
                 loss_msg = ''.join(['{:15,.4f}'.format(np.mean(game)) for game in self.loss])
                 print('\r\tloss (average)'.ljust(18), loss_msg)
+                self.model.save_checkpoint(self.save_dir, episode, steps * self.instances)
 
             actions = actions_new
 

core/memory/sampling/base_gym.py
@@ -24,16 +24,20 @@ class BaseSamplingGym(AbstractSampling, Sequence):
         gamma: float
             Discount factor for the reinforcement learning update rule.
 
+        alpha: float
+            Learning rate for the target
+
         batch_size: int
             The size of one batch
     """
 
-    def __init__(self, replay_memory, model, gamma, batch_size: int = 64):
+    def __init__(self, replay_memory, model, gamma, alpha=0.1, batch_size: int = 64):
         super().__init__(replay_memory, batch_size)
         self.replay_memory = replay_memory
         self.batch_size = batch_size
         self.model = model
         self.gamma = gamma
+        self.alpha = alpha
 
     def __len__(self):
         return len(self.replay_memory) // self.batch_size
@@ -56,7 +60,7 @@ class BaseSamplingGym(AbstractSampling, Sequence):
         for k in range(batch_size):
             target = rewards[k]
             if not done[k]:
-                target = rewards[k] + 0.1 * (self.gamma * np.amax(q_values_next[k]))
+                target = rewards[k] + self.alpha * (self.gamma * np.amax(q_values_next[k]))
 
             target_f = q_values[k]
             target_f[actions[k]] = target

core/memory/sampling/base_multi_env.py
@@ -19,10 +19,13 @@ class BaseSamplingMultiEnv(AbstractSampling, Sequence):
             The size of one batch
     """
 
-    def __init__(self, replay_memory, batch_size: int = 64):
+    def __init__(self, replay_memory, model, alpha, gamma, batch_size: int = 64):
         super().__init__(replay_memory, batch_size)
         self.replay_memory = replay_memory
         self.batch_size = batch_size
+        self.model = model
+        self.alpha = alpha
+        self.gamma = gamma
 
     def __len__(self):
         return len(self.replay_memory) // self.batch_size
@@ -30,17 +33,36 @@ class BaseSamplingMultiEnv(AbstractSampling, Sequence):
     def __getitem__(self, item):
         if item >= len(self):
             raise StopIteration
-
-        sample_idx = np.random.randint(0, len(self.replay_memory), self.batch_size)
-        model_input, model_output = self.replay_memory.get_batch(sample_idx)
-        return self.reformat_states(model_input), model_output
+        return self.random_batch(self.batch_size)
 
     def random_batch(self, batch_size: int = None):
         """  Returns a single random batch.  """
         batch_size = self.batch_size if batch_size is None else batch_size
-        sample_idx = np.random.randint(0, len(self.replay_memory), batch_size)
-        model_input, model_output = self.replay_memory.get_batch(sample_idx)
-        return self.reformat_states(model_input), model_output
+        sample_idx = np.random.randint(0, len(self.replay_memory)-1, batch_size)
+
+        states, actions, rewards, done, states_next = self.replay_memory.get_batch(sample_idx)
+        model_input, model_output = self._create_model_input(states, actions, rewards, done, states_next)
+        return model_input, model_output
+
+    def _create_model_input(self, states, actions, rewards, done, states_next):
+        states = self.reformat_states(states)
+        states_next= self.reformat_states(states_next)
+
+        q_values = self.model.predict(states)
+        q_values_next = self.model.predict(states_next)
+
+        targets = []
+        for k in range(self.batch_size):
+            target = rewards[k]
+            if not done[k]:
+                target = rewards[k] + self.alpha * (self.gamma * np.amax(q_values_next[k]))
+
+            target_f = q_values[k]
+            target_f[actions[k]] = target
+            targets.append(target_f)
+
+        model_input, model_output = states, np.array(targets)
+        return model_input, model_output
 
     @staticmethod
     def reformat_states(states):